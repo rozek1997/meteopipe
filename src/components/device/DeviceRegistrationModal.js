3d94adf152873d5b9ed66d7b5c30639d68749ad5
rozek1997 <marek.roskowicz@student.wat.edu.pl>
2021-01-09 14:40:05 +0100

    Sepearte modal code for registering new devices from devices subpage code

---
+++ src/components/device/DeviceRegistrationModal.js
@@ -0,0 +1,134 @@
+import React from "react"
+import Input from "../form/Input";
+import {api} from "../../api/axios";
+import "./DeviceRegistrationModal.css"
+
+class DeviceRegistrationModal extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.popupRef = React.createRef();
+        this.state = {
+            newDevice: {
+                name: "",
+                message: "",
+                certURL: ""
+            }
+        }
+    }
+
+
+    onRegisterDeviceInputChange = (event) => {
+        const name = event.target.value;
+        this.setState(prevState => ({
+            newDevice: {
+                ...prevState.newDevice,
+                name,
+            }
+
+        }));
+    }
+
+    addNewDevices = async () => {
+        const {name} = this.state.newDevice;
+        let response = undefined
+        if (name.length !== 0) {
+            try {
+                response = await api.registerDevice(name, "cb83d2e9-e212-463d-9b5e-25b2de315377");
+                console.log(response)
+                this.setState(prevState => ({
+                    newDevice: {
+                        ...prevState.newDevice,
+                        name: "",
+                        message: "Device has been successfully provisioned. Copy the link by clicking the button below",
+                        certURL: response.data["get_file_url"]
+                    }
+                }));
+                await this.props.reloadList("cb83d2e9-e212-463d-9b5e-25b2de315377");
+
+            } catch (e) {
+                this.setState(prevState => ({
+                    newDevice: {
+                        ...prevState.newDevice,
+                        name: "",
+                        message: `Something went wrong, error: ${e}`
+                    }
+
+                }));
+            }
+        } else {
+            this.setState(prevState => ({
+                newDevice: {
+                    ...prevState.newDevice,
+                    message: "Provide name",
+                }
+            }));
+        }
+
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        this.onVisibilityPropsChnage();
+    }
+
+    onVisibilityPropsChnage = () => {
+        const visible = this.props.visible;
+        if (visible)
+            this.displayModal();
+        else
+            this.hideModal();
+    }
+
+    displayModal = () => {
+        this.popupRef.current.style.visibility = "visible"
+    }
+
+    hideModal = () => {
+        this.popupRef.current.style.visibility = "hidden"
+    }
+
+    renderClipBoardButton = () => {
+        return (
+            <React.Fragment>
+                <button className="clipboard__button" onClick={this.copyToClipBoard}>Copy url to
+                    clipboard
+                </button>
+            </React.Fragment>
+        );
+    }
+
+    copyToClipBoard = async () => {
+        const downloadURL = this.state.newDevice.certURL;
+        try {
+            await navigator.clipboard.writeText(downloadURL);
+        } catch (e) {
+            console.log("Failed to copy", e)
+        }
+    }
+
+    render() {
+
+        const device = this.state.newDevice;
+        return (
+            <div className="device__popup" onClick={this.hideModal} ref={this.popupRef}>
+                <div className="device__form" onClick={event => event.stopPropagation()}>
+                    <h3 className="form__title">Register device</h3>
+                    <p className="form__description">Provide thing name. App will provisioned new device and return
+                        link.
+                        After that you have 10 minutes to download cert and keys which enable you connect to the
+                        cloud</p>
+                    <Input type="text" hint="device name" value={this.state.newDevice.name}
+                           onChange={this.onRegisterDeviceInputChange}
+                           name="name"/>
+                    <button className="form__button" onClick={this.addNewDevices}>Register device</button>
+                    <p className="form__message">{device.message}</p>
+                    {/*{this.renderClipBoardButton(device.certURL)}*/}
+                    {device.certURL.length > 0 ? this.renderClipBoardButton() : null}
+                </div>
+            </div>
+        );
+    }
+}
+
+
+export default DeviceRegistrationModal;
